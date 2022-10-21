5acaea77eb22f772e5f3587bd6d5f7741febe8fc
uyudane <sy.yun@icloud.com>
2022-10-21 09:57:53 +0900

    Add: ユーザーっとロードマップの簡易的なseedを作成

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+3.times do |n|
+  user = User.new(
+    sub: "12345_#{n+1}",
+  )
+
+  5.times do |m|
+    user.roadmaps.build(
+      title: "ロードマップ_#{n+1}",
+      introduction: "説明",
+    )
+  end
+
+  user.save!
+end
