af7c2bb26df27fbb46398e51809fe6bfcbea4c52
uyudane <sy.yun@icloud.com>
2022-10-28 14:23:01 +0900

    Add: userのnameにバリデーションを追加

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :roadmaps, dependent: :destroy
 
+  validates :name, presence:true
+
   def self.from_token_payload(payload)
     # すでにユーザ情報があった場合はその情報を取得
     # ユーザ登録をしたタイミングの場合は、ユーザテーブルにsubとユーザ名を追加
