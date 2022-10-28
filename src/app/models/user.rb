f8adca405b9258a4679a007d932c600a6582d8ee
uyudane <sy.yun@icloud.com>
2022-10-28 15:57:26 +0900

    Add: subにtwitter等の外部認証情報を含めない形で登録するように修正

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   def self.from_token_payload(payload)
     # すでにユーザ情報があった場合はその情報を取得
     # ユーザ登録をしたタイミングの場合は、ユーザテーブルにsubとユーザ名を追加
-    find_by(sub: payload['sub']) || create!(sub: payload['sub'],name: payload['.name'])
+    find_by(sub: payload['sub'].split("|")[1]) || create!(sub: payload['sub'].split("|")[1],name: payload['.name'])
   end
 end
