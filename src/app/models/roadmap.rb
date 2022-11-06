46bbf01e61c59f496e3a3b48f2676d3357cdcee2
uyudane <sy.yun@icloud.com>
2022-11-06 14:27:36 +0900

    Add: タグを新規作成時に登録できるように修正

--- src/app/models/roadmap.rb
+++ src/app/models/roadmap.rb
@@ -2,4 +2,14 @@ class Roadmap < ApplicationRecord
   belongs_to :user
   has_many :roadmap_tags, dependent: :destroy
   has_many :tags, through: :roadmap_tags
+
+  def save_with_tags(tag_list)
+    ActiveRecord::Base.transaction do
+      self.tags = tag_list.map { |name| Tag.find_or_initialize_by(name: name.strip) }
+      save!
+    end
+    true
+  rescue StandardError
+    false
+  end
 end
