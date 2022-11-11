3de49cfdbf3dde288ce661bf354266b4e5598129
uyudane <sy.yun@icloud.com>
2022-11-11 20:56:07 +0900

    Add: likeでsubを返すようにserializerを修正

--- src/app/serializers/like_serializer.rb
+++ src/app/serializers/like_serializer.rb
@@ -1,6 +1,10 @@
 class LikeSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :roadmap_id
+  attributes :id, :roadmap_id, :user_sub
 
   belongs_to :user, serializer: UserSerializer
   belongs_to :roadmap, serializer: RoadmapSerializer
+
+  def user_sub
+    object.user.sub
+  end
 end
