7be5b294b5c6c1d74f02df1219e3834e693de1a7
uyudane <sy.yun@icloud.com>
2022-11-17 16:18:33 +0900

    Add: いいね数を連携できるように修正

--- src/app/serializers/roadmap_serializer.rb
+++ src/app/serializers/roadmap_serializer.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class RoadmapSerializer < ActiveModel::Serializer
-  attributes :id, :title, :introduction, :created_at, :start_skill, :end_skill, :is_published
+  attributes :id, :title, :introduction, :created_at, :start_skill, :end_skill, :is_published, :number_of_likes
 
   belongs_to :user, serializer: UserSerializer
   has_many :tags, serializer: TagSerializer
   has_many :steps, serializer: StepSerializer
   has_many :likes, serializer: LikeSerializer
+
+  def number_of_likes
+    object.likes.length
+  end
+
 end
