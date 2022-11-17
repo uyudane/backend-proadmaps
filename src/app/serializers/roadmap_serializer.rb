77ec0e6d9993f4ace42ef3858ed357498ba7d8da
uyudane <sy.yun@icloud.com>
2022-11-17 20:32:01 +0900

    Add: ロードマップ作成タイミングを年月日で出力できるようにした

--- src/app/serializers/roadmap_serializer.rb
+++ src/app/serializers/roadmap_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RoadmapSerializer < ActiveModel::Serializer
-  attributes :id, :title, :introduction, :created_at, :start_skill, :end_skill, :is_published, :number_of_likes
+  attributes :id, :title, :introduction, :created_date, :start_skill, :end_skill, :is_published, :number_of_likes
 
   belongs_to :user, serializer: UserSerializer
   has_many :tags, serializer: TagSerializer
@@ -9,7 +9,11 @@ class RoadmapSerializer < ActiveModel::Serializer
   has_many :likes, serializer: LikeSerializer
 
   def number_of_likes
-    object.likes.length
+    object.likes.length.to_s
+  end
+
+  def created_date
+    object.created_at.strftime('%Y年%m月%d日')
   end
 
 end
