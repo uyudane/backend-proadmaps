7562770beec29fc1c637bf97dd3329e705a397ac
uyudane <sy.yun@icloud.com>
2022-12-16 21:10:59 +0900

    Add: factorybotを追加

--- src/config/application.rb
+++ src/config/application.rb
@@ -29,7 +29,6 @@ module App
       g.helper false
       g.test_framework false
       g.test_framework :rspec,
-                       fixtures: false,
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false
